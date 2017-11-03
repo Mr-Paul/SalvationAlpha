a44fdd0f130e55f67d7f4ba901babdf8121e31f3
Mr-Paul <heppner.paul@gmail.com>
2017-11-02 21:00:26 -0400

    baseline

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCController : MonoBehaviour {
+
+	private Vector3 myPosition;
+	private GameObject[] resources;
+	private NavMeshAgent nav;
+	private GameObject closest;
+	private float distance;
+
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	
+	void Update () {
+		resources = GameObject.FindGameObjectsWithTag ("Resource");
+		if (resources.Length > 0) {
+			closest = null;
+			distance = Mathf.Infinity;
+			foreach (GameObject resource in resources) {
+				Vector3 diff = resource.transform.position - transform.position;
+				float currDistance = diff.sqrMagnitude;
+				if (currDistance < distance) {
+					closest = resource;
+					distance = currDistance;
+				}
+			}
+			nav.isStopped = false;
+			nav.SetDestination (closest.transform.position);
+		} else {
+			nav.isStopped = true;
+		}
+	}
+}
---
+++ Assets/Scripts/TreeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour {
+
+	private bool indicatorActive;
+	private float proximity;
+	private GameObject player;
+	public GameObject indicator;
+	private GameObject thisIndicator;
+
+	void Start () {
+		indicatorActive = false;
+		proximity = 3;
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	void Update () {
+		if ((Vector3.Distance (transform.position, player.transform.position) <= proximity)) {
+			if (!indicatorActive) {
+				thisIndicator = Instantiate (indicator, transform);
+				indicatorActive = true;
+			}
+			if (Input.GetKey(KeyCode.E)) {
+				DestroyObject(gameObject);
+			}
+		} else {
+			if (indicatorActive) {
+				Destroy(thisIndicator);
+				indicatorActive = false;
+			}
+		}
+	}
+}
