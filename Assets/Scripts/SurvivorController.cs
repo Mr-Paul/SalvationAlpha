0c27633e3a0c80a9c39647b0c1f5b40c87ea9eee
Mr-Paul <heppner.paul@gmail.com>
2017-11-21 18:05:43 -0500

    carrying capacity, resource drop off points

--- Assets/Scripts/SurvivorController.cs
+++ Assets/Scripts/SurvivorController.cs
@@ -7,19 +7,27 @@ public class SurvivorController : MonoBehaviour {
 
 	private Vector3 myPosition;
 	private GameObject[] resources;
+	GameObject[] dropOffPoints;
 	private NavMeshAgent nav;
-	private GameObject closest;
-	private float distance;
+	private GameObject target;
 	private GameController gameController;
+	int capacity;
+	int maxCapacity;
 	public int harvestQuantity;
 	private float harvestDelay;
 	private float harvestNext;
 
 	void Start () {
+		
 		harvestQuantity = 10;
 		harvestDelay = 1.0f;
 		harvestNext = 0.5f;
+
+		capacity = 0;
+		maxCapacity = 85;
+
 		nav = GetComponent<NavMeshAgent> ();
+		
 		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
 		if (gameControllerObject != null)
 		{
@@ -29,27 +37,29 @@ public class SurvivorController : MonoBehaviour {
 		{
 			Debug.Log ("Cannot find 'GameController' script");
 		}
+
 	}
 	
 	void Update () {
-		
+
+		dropOffPoints = GameObject.FindGameObjectsWithTag ("Drop Off");
 		resources = GameObject.FindGameObjectsWithTag ("Resource");
-		if (resources.Length > 0) {
-			closest = null;
-			distance = Mathf.Infinity;
-			foreach (GameObject resource in resources) {
-				Vector3 diff = resource.transform.position - transform.position;
-				float currDistance = diff.sqrMagnitude;
-				if (currDistance < distance) {
-					closest = resource;
-					distance = currDistance;
-				}
-			}
+		if (resources.Length > 0 && capacity < maxCapacity) {
+			target = GetTarget (resources);
 			nav.isStopped = false;
-			nav.SetDestination (closest.transform.position);
+			nav.SetDestination (target.transform.position);
 			if (nav.remainingDistance <= nav.stoppingDistance && !nav.pathPending) {
 				if (nav.velocity.sqrMagnitude == 0.0f) {
-					Harvest (closest);
+					Harvest (target);
+				}
+			}
+		} else if (dropOffPoints.Length > 0) {
+			target = GetTarget (dropOffPoints);
+			nav.isStopped = false;
+			nav.SetDestination (target.transform.position);
+			if (nav.remainingDistance <= nav.stoppingDistance && !nav.pathPending) {
+				if (nav.velocity.sqrMagnitude == 0.0f) {
+					DropOff ();
 				}
 			}
 		} else {
@@ -59,11 +69,36 @@ public class SurvivorController : MonoBehaviour {
 
 	void Harvest (GameObject resource) {
 		if (Time.time >= harvestNext) {
+			int thisHarvest;
+			if (capacity + harvestQuantity < maxCapacity) {
+				thisHarvest = harvestQuantity;
+			} else {
+				thisHarvest = maxCapacity - capacity;
+			}
 			ResourceController resourceController = resource.GetComponent<ResourceController> ();
 			string type = resourceController.GetResourceType ();
-			int gathered = resourceController.HarvestResource (10);
-			gameController.ResourceChange (type, gathered);
+			int gathered = resourceController.HarvestResource (thisHarvest);
+			capacity += gathered;
 			harvestNext = Time.time + harvestDelay;
 		}
+	}
+
+	void DropOff() {
+		gameController.ResourceChange ("wood", capacity);
+		capacity = 0;
+	}
+
+	GameObject GetTarget (GameObject[] targets) {
+		GameObject closest = null;
+		float distance = Mathf.Infinity;
+		foreach (GameObject target in targets) {
+			Vector3 diff = target.transform.position - transform.position;
+			float currDistance = diff.sqrMagnitude;
+			if (currDistance < distance) {
+				closest = target;
+				distance = currDistance;
+			}
+		}
+		return closest;
 	}
 }
